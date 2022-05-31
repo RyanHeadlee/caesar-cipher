9b624b28324a9e6e80313009813697f5d4e183fb
Ryan Headlee <headleeryan@gmail.com>
2022-05-31 14:02:23 -0700

    Reduce caesar_cipher to single function call

--- script.rb
+++ script.rb
@@ -1,56 +1,14 @@
-def caesar_cipher(string, shift = 0)
-  shift = rand(1..25) if shift > 25 || shift < -25
-  code_array = string.codepoints.map do |code|
-    next code if skip?(code)
-    if code.between?(65, 90)
-      next upcase_shift(code, shift)
-    elsif code.between?(97, 122)
-      next downcase_shift(code, shift)
-    elsif code.between?(49, 57)
-      number_shift(code, shift)
+def caesar_cipher(string, shift, result = "")
+  string.each_char do |char|
+    base = char.ord < 91 ? 65 : 97
+    if char.ord.between?(65 ,90) || char.ord.between?(97, 122)
+      rotation = (((char.ord - base) + shift) % 26) + base
+      result += rotation.chr
+    else
+      result += char
     end
   end
-  code_array.map { |code| code = code.chr }.join("")
-end
-
-def upcase_shift(code, shift)
-  if code + shift > 90
-    code = (code + shift) - (90 - 64)
-  elsif code + shift < 65
-    code = (code + shift) + (90 - 64)
-  else
-    code += shift
-  end
-end
-
-def downcase_shift(code, shift)
-  if code + shift > 122
-    code = (code + shift) - (122 - 96)
-  elsif code + shift < 97
-    code = (code + shift) + (122 - 96)
-  else
-    code += shift
-  end
-end
-
-def number_shift(code, shift)
-  shift = rand(1..9) if shift > 9 || shift < -9
-  if code + shift > 57
-    code = (code + shift) - (57 - 48)
-  elsif code + shift < 49
-    code = (code + shift) + (57 - 48)
-  else
-    code += shift
-  end
-end
-
-def skip?(code)
-  if code.between?(0, 48) || code.between?(58, 64) ||
-     code.between?(91, 96) || code > 122
-    true
-  else
-    false
-  end
+  result
 end
 
 def prompt(string)
@@ -58,4 +16,4 @@ def prompt(string)
   gets.chomp
 end
 
-p caesar_cipher(prompt("Enter your phrase: ").to_s, prompt("Enter you shift: ").to_i)
+puts caesar_cipher(prompt("Enter your phrase: ").to_s, prompt("Enter you shift: ").to_i)
