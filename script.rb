d4cc6ffc864322de9e50e92a2012f063786ea39c
Ryan Headlee <headleeryan@gmail.com>
2022-05-31 17:15:59 -0700

    Add code to allow numbers to be shifted

--- script.rb
+++ script.rb
@@ -1,8 +1,11 @@
 def caesar_cipher(string, shift, result = "")
   string.each_char do |char|
-    base = char.ord < 91 ? 65 : 97
+    base = char.ord.between?(65, 90) ? 65 : char.ord.between?(97, 122) ? 97 : 49
     if char.ord.between?(65 ,90) || char.ord.between?(97, 122)
       rotation = (((char.ord - base) + shift) % 26) + base
+      result += rotation.chr
+    elsif char.ord.between?(49, 57)
+      rotation = (((char.ord - base) + shift) % 9) + base
       result += rotation.chr
     else
       result += char
